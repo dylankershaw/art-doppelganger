2e8705be7489d7f1002f9f68f27bb76912a00cbc
github username <dylankershaw@me.com>
2018-01-16 12:13:21 -0500

    working webcam and screenshot button

--- src/components/webcam_container.js
+++ src/components/webcam_container.js
@@ -1,5 +1,13 @@
-import React from "react";
+import { connect } from "react-redux";
 import Webcam from "react-webcam";
+import React from "react";
+
+import { getDogpelganger } from "../actions";
+
+const handleClick = ({ getDogpelganger }) => {
+  const screenshot = this.webcam.getScreenshot();
+  getDogpelganger(screenshot);
+};
 
 const WebcamContainer = () => {
   return (
@@ -10,8 +18,9 @@ const WebcamContainer = () => {
         screenshotFormat="image/jpeg"
         height="300"
       />
+      <button onClick={handleClick}>TAKE PHOTO</button>
     </div>
   );
 };
 
-export default WebcamContainer;
+export default connect(null, { getDogpelganger })(WebcamContainer);
